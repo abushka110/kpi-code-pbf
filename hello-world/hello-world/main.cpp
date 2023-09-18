173233819408e307da5bead859200b697a12a801
Oleh Donets <qyjodixilu@gmail.com>
2023-09-18 21:14:59 +0300

    gitignore update

---
+++ hello-world/hello-world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by Oleh Donets on 18/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
