7b5012fda52211deb8f51d3dae29fd3d07ded62f
Oleh Donets Win <qyjodixilu@gmail.com>
2023-10-12 04:02:20 -0700

    practical 4 test

---
+++ practical-4/1test/Main.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef MainH
+#define MainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TMaiForm : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TMaiForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TMaiForm *MaiForm;
+//---------------------------------------------------------------------------
+#endif
