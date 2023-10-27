eecb55649d872a481b11690adeff8a5bf395d513
Oleh Donets <qyjodixilu@gmail.com>
2023-10-27 18:19:11 +0300

    comments added

--- practical-6/task2-Console-switch/main.cpp
+++ practical-6/task2-Console-switch/main.cpp
@@ -1,29 +1,32 @@
-#include <iostream>
-#include <cmath>
+#include <iostream>  // Підключення бібліотеки для введення/виведення на консоль.
+#include <cmath>     // Підключення бібліотеки для математичних функцій.
 
 double CalculateAlphaProfit(double x) {
+    // Обчислення прибутку для підрозділу "Alpha" на основі функції зі складними математичними виразами.
     return 100 * fabs(cos(fabs(2 * x) / 2.15 - cos(3 * x - 3) + 10) + 100);
 }
 
 double CalculateBetaProfit(double x) {
+    // Обчислення прибутку для підрозділу "Beta" на основі функції зі складними математичними виразами.
     return 120 * fabs(sin(x) * sin(x + 2) * cos(x * x) / 0.44 + 11.4 + 120);
 }
 
 double CalculateGammaProfit(double x) {
+    // Обчислення прибутку для підрозділу "Gamma" на основі функції зі складними математичними виразами.
     return 130 * fabs(fabs(3 * sin(3 * x) + sin(2 * x - 1.8)) + 3.45) + 130;
 }
 
 int main() {
-    double x = 5;
-    int i;
+    double x = 5;  // Ініціалізація змінної x значенням 5.
+    int i;  // Оголошення змінної i для вибору підрозділу.
 
-    std::cout << "Введіть номер підрозділу (1 - alpha, 2 - betta або 3 - lamda): ";
-    std::cin >> i;
+    std::cout << "Введіть номер підрозділу (1 - alpha, 2 - beta або 3 - lamda): ";
+    std::cin >> i;  // Зчитування номера підрозділу від користувача.
 
-    double profit;
-    double depreciation;
+    double profit;  // Змінна для обчисленого прибутку.
+    double depreciation;  // Змінна для обчислення амортизації.
 
-    switch (i) {
+    switch (i) {  // Розгалуження в залежності від вибору користувача.
         case 1:
             profit = CalculateAlphaProfit(x);
             depreciation = 0.2 * profit;
@@ -38,12 +41,12 @@ int main() {
             break;
         default:
             std::cout << "Неправильний номер варіанту. Введіть 1, 2 або 3." << std::endl;
-            return 1;
+            return 1;  // Вихід з програми у разі некоректного вибору підрозділу.
     }
 
-    std::cout << "Прибуток: " << profit << std::endl;
-    std::cout << "Затрати на амортизацію: " << depreciation << std::endl;
-    std::cout << "Загальний прибуток: " << profit - depreciation << std::endl;
+    std::cout << "Прибуток: " << profit << std::endl;  // Виведення обчисленого прибутку.
+    std::cout << "Затрати на амортизацію: " << depreciation << std::endl;  // Виведення амортизації.
+    std::cout << "Загальний прибуток: " << profit - depreciation << std::endl;  // Виведення загального прибутку.
 
-    return 0;
+    return 0;  // Завершення програми без помилок.
 }
