9549d072c3441f1c3207ecd9c014a213b62eb95a
Oleh Donets <qyjodixilu@gmail.com>
2023-10-27 17:35:36 +0300

    comments added

--- practical-6/task1-Console/main.cpp
+++ practical-6/task1-Console/main.cpp
@@ -1,27 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <iostream>  // Включення бібліотеки для виводу та введення даних
+#include <vector>    // Включення бібліотеки для роботи з векторами
+#include <string>    // Включення бібліотеки для роботи з рядками
+#include <sstream>   // Включення бібліотеки для роботи з рядковими потоками
 
-using namespace std;
-
-void totalSum(const vector<pair<string, int>>& menu, const vector<int>& order, int total) {
-    if (order.size() == menu.size()) {
-        total = total - (total * 5 / 100); // Знижка 5%
-        cout << "До сплати (з урахуванням знижки): " << total << "грн." << endl;
-    } else {
-        cout << "До сплати: " << total << "грн." << endl;
-    }
-}
-
-int getUserPayChoice() {
-    int choice;
-    cout << "Оплатити замовлення? (Так - 1; Ні - 2): ";
-    cin >> choice;
-    return choice;
-}
+using namespace std;  // Використання простору імен "std"
 
 int main() {
+    // Оголошення меню з парами "назва продукту" та "ціна"
     vector<pair<string, int>> menu = {
             {"напій", 34},
             {"морозиво", 63},
@@ -30,14 +15,14 @@ int main() {
             {"бургер", 70}
     };
 
-    vector<int> order;
-    int total = 0;
-    bool burgerOrFriesSelected = false;
+    vector<int> order;  // Вектор для зберігання замовлення
+    int total = 0;  // Загальна вартість замовлення
 
-    while (true) {
+    while (true) {  // Безкінечний цикл для обробки замовлень
         cout << "Зробіть замовлення:" << endl;
+
         for (int i = 0; i < menu.size(); i++) {
-            // Виводимо всі товари окрім соусу, якщо в корзині немає картоплі фрі або бургера
+            // Виведення позицій меню та їх ціни, окрім соусу
             if (!((order.empty() || (find(order.begin(), order.end(), 3) == order.end())) && (i == 3))) {
                 cout << i + 1 << ". " << menu[i].first << "(" << menu[i].second << "грн), ";
             }
@@ -46,21 +31,21 @@ int main() {
         cout << endl;
 
         string input;
-        getline(cin, input);
-        stringstream ss(input);
+        getline(cin, input);  // Отримання введення від користувача
+        stringstream ss(input);  // Створення рядкового потоку для розбору введення
         int item;
 
-        while (ss >> item) {
+        while (ss >> item) {  // Парсинг введення користувача
             if (item >= 1 && item <= menu.size()) {
-                order.push_back(item);
-                total += menu[item - 1].second;
-                if (item == 3 || item == 5) { // Перевіряємо, чи вибрали бургер або картоплю фрі
+                order.push_back(item);  // Додавання вибраних позицій до замовлення
+                total += menu[item - 1].second;  // Обчислення загальної вартості замовлення
+                if (item == 3 || item == 5) {
                     int sauceChoice;
                     cout << "Чи потрібен соус (Так - 1; Ні - 2): ";
-                    cin >> sauceChoice;
+                    cin >> sauceChoice;  // Вибір користувача щодо соусу
                     if (sauceChoice == 1) {
-                        order.push_back(4); // Додаємо соус до замовлення
-                        total += menu[3].second;
+                        order.push_back(4);  // Додавання соусу до замовлення
+                        total += menu[3].second;  // Оновлення загальної вартості замовлення
                     }
                 }
             }
@@ -68,27 +53,24 @@ int main() {
 
         cout << "Ви обрали: ";
         for (int i = 0; i < order.size(); i++) {
-            cout << menu[order[i] - 1].first;
+            cout << menu[order[i] - 1].first;  // Виведення обраних позицій з меню
             if (i < order.size() - 1) {
                 cout << ", ";
             }
         }
-        cout << "\nЗ вас: " << total << "грн." << endl;
-        int choice = getUserPayChoice();
+        cout << endl;
 
-        if (choice == 1) {
-            if (total > 0) {
-                totalSum(menu, order, total);
-            }
-            break;
-        } else if (choice == 2) {
-            cout << "Прикро, що ви відмінили замовлення. До побачення!" << endl;
-            break;
+        // перевірка, чи користувач замовив усе меню
+        if (order.size() == menu.size()) {
+            total = total - (total * 5 / 100);  // Знижка 5% за весь заказ
+            cout << "До сплати (з урахуванням знижки): " << total << "грн." << endl;
+        } else {
+            cout << "До сплати: " << total << "грн." << endl;
+            break;  // Завершення програми, замовлення закінчено
         }
 
-        order.clear();
-        total = 0;
+        order.clear();  // Очищення замовлення
+        total = 0;  // Скидання загальної вартості
     }
-
-    return 0;
+    return 0;  // Завершення програми
 }
--- practical-6/task1-GUI/Unit1.cpp
+++ practical-6/task1-GUI/Unit1.cpp
@@ -8,8 +8,8 @@
 #pragma resource "*.dfm"
 TForm1 *Form1;
 
-__fastcall TForm1::TForm1(TComponent* Owner)
-	: TForm(Owner)
+__fastcall TForm1::TForm1(TComponent *Owner)
+		: TForm(Owner)
 {
 	// Встановлюємо початкової властивість Enabled для CheckBox4 залежно від стану CheckBox3 та CheckBox5
 	CheckBox4->Enabled = CheckBox3->Checked || CheckBox5->Checked;
@@ -32,45 +32,54 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 	// Оголошення змінної для обчислення загальної суми покупки
 	double totalSum = 0.0;
 
-    // Додавання вартості напою до загальної суми, якщо вибрано CheckBox1
-    switch (CheckBox1->Checked)
-    {
-		case true: totalSum += 34.0; // Вартість напою
-            break;
-		default: break;
-    }
+	// Додавання вартості напою до загальної суми, якщо вибрано CheckBox1
+	switch (CheckBox1->Checked)
+	{
+	case true:
+		totalSum += 34.0; // Вартість напою
+		break;
+	default:
+		break;
+	}
 
-    // Додавання вартості морозива до загальної суми, якщо вибрано CheckBox2
-    switch (CheckBox2->Checked)
-    {
-		case true: totalSum += 63.0; // Вартість морозива
-            break;
-		default: break;
-    }
+	// Додавання вартості морозива до загальної суми, якщо вибрано CheckBox2
+	switch (CheckBox2->Checked)
+	{
+	case true:
+		totalSum += 63.0; // Вартість морозива
+		break;
+	default:
+		break;
+	}
 
 	// Додавання вартості картоплі до загальної суми, якщо вибрано CheckBox3
 	switch (CheckBox3->Checked)
 	{
-		case true: totalSum += 51.0; // Вартість картоплі
-			break;
-		default: break;
+	case true:
+		totalSum += 51.0; // Вартість картоплі
+		break;
+	default:
+		break;
 	}
 
 	// Додавання вартості кетчупу до загальної суми, якщо вибрано CheckBox4
 	switch (CheckBox4->Checked)
 	{
-		case true: totalSum += 16.0; // Вартість кетчупу
-			break;
-		default: break;
+	case true:
+		totalSum += 16.0; // Вартість кетчупу
+		break;
+	default:
+		break;
 	}
 
 	// Додавання вартості пиріжка до загальної суми, якщо вибрано CheckBox5
 	switch (CheckBox5->Checked)
 	{
-		case true:
-			totalSum += 70.0; // Вартість пиріжка
-			break;
-		default: break;
+	case true:
+		totalSum += 70.0; // Вартість пиріжка
+		break;
+	default:
+		break;
 	}
 
 	// Застосовуємо знижку 5% до загальної суми, якщо всі CheckBox'и вибрані
@@ -87,4 +96,3 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 }
 
 //---------------------------------------------------------------------------
-
