3ffe17df108188750ceb7557b57cd2bde0487110
Oleh Donets Win <qyjodixilu@gmail.com>
2023-10-27 19:26:05 -0700

    comments added

--- practical-6/task2-GUI-switch/Unit1.cpp
+++ practical-6/task2-GUI-switch/Unit1.cpp
@@ -1,6 +1,4 @@
-﻿//---------------------------------------------------------------------------
-
-#include <vcl.h>
+﻿#include <vcl.h>
 #pragma hdrstop
 
 #include "Unit1.h"
@@ -9,6 +7,7 @@
 #pragma resource "*.dfm"
 TForm1 *Form1;
 //---------------------------------------------------------------------------
+
 #include <cmath>
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
@@ -20,10 +19,12 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 double CalculateAlphaProfit(double x) {
 	return 100 * fabs(cos(fabs(2 * x) / 2.15 - cos(3 * x - 3) + 10) + 100); // Обчислюємо прибуток для підрозділу alpha
 }
+
 // Функція для обчислення прибутку для підрозділу beta
 double CalculateBetaProfit(double x) {
 	return 120 * fabs(sin(x) * sin(x + 2) * cos(x * x) / 0.44 + 11.4 + 120); // Обчислюємо прибуток для підрозділу beta
 }
+
 // Функція для обчислення прибутку для підрозділу gamma
 double CalculateGammaProfit(double x) {
 	return 130 * fabs(fabs(3 * sin(3 * x) + sin(2 * x - 1.8)) + 3.45) + 130; // Обчислюємо прибуток для підрозділу gamma
@@ -34,8 +35,6 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 	double x = 5;
 	double profit;
 	double depreciation;
-
-
 
 	switch (RadioGroup1->ItemIndex) {  // Розгалуження в залежності від вибору користувача.
 		case 0:
@@ -56,12 +55,12 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 
 	double fullSum = profit - depreciation;
 	Label2->Caption = "Profit: " + FloatToStr(profit)  + "\nDepreciation costs: " + FloatToStr(depreciation) + "\nTotal profit: " + FloatToStr(fullSum);
-
-
 }
 //---------------------------------------------------------------------------
+
 void __fastcall TForm1::Button2Click(TObject *Sender)
 {
 	Close();
 }
 //---------------------------------------------------------------------------
+
