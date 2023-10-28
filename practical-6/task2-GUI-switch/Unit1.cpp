b37b1bb4ac8e69c8c5b2c8734a0262b48980b933
Oleh Donets Win <qyjodixilu@gmail.com>
2023-10-27 19:22:40 -0700

    practical 6 task 2 g switch

---
+++ practical-6/task2-GUI-switch/Unit1.cpp
@@ -0,0 +1,67 @@
+﻿//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+#include <cmath>
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+// Функція для обчислення прибутку для підрозділу alpha
+double CalculateAlphaProfit(double x) {
+	return 100 * fabs(cos(fabs(2 * x) / 2.15 - cos(3 * x - 3) + 10) + 100); // Обчислюємо прибуток для підрозділу alpha
+}
+// Функція для обчислення прибутку для підрозділу beta
+double CalculateBetaProfit(double x) {
+	return 120 * fabs(sin(x) * sin(x + 2) * cos(x * x) / 0.44 + 11.4 + 120); // Обчислюємо прибуток для підрозділу beta
+}
+// Функція для обчислення прибутку для підрозділу gamma
+double CalculateGammaProfit(double x) {
+	return 130 * fabs(fabs(3 * sin(3 * x) + sin(2 * x - 1.8)) + 3.45) + 130; // Обчислюємо прибуток для підрозділу gamma
+}
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	double x = 5;
+	double profit;
+	double depreciation;
+
+
+
+	switch (RadioGroup1->ItemIndex) {  // Розгалуження в залежності від вибору користувача.
+		case 0:
+            profit = CalculateAlphaProfit(x);
+            depreciation = 0.2 * profit;
+            break;
+		case 1:
+            profit = CalculateBetaProfit(x);
+            depreciation = 0.3 * profit;
+            break;
+		case 2:
+            profit = CalculateGammaProfit(x);
+            depreciation = 0.4 * profit;
+            break;
+		default:
+			ShowMessage("Неправильний номер варіанту. Введіть 1, 2 або 3.");
+	}
+
+	double fullSum = profit - depreciation;
+	Label2->Caption = "Profit: " + FloatToStr(profit)  + "\nDepreciation costs: " + FloatToStr(depreciation) + "\nTotal profit: " + FloatToStr(fullSum);
+
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+	Close();
+}
+//---------------------------------------------------------------------------
---
+++ practical-6/task2-GUI-switch/Unit1.dfm
@@ -0,0 +1,58 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 24
+    Top = 24
+    Width = 266
+    Height = 15
+    Caption = 'Program for calculating profit by various divisions:'
+  end
+  object Label2: TLabel
+    Left = 24
+    Top = 184
+    Width = 34
+    Height = 15
+    Caption = 'Label2'
+  end
+  object RadioGroup1: TRadioGroup
+    Left = 24
+    Top = 64
+    Width = 266
+    Height = 105
+    Caption = 'Choose division:'
+    Items.Strings = (
+      'Alpha'
+      'Betta'
+      'Lamda')
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 24
+    Top = 304
+    Width = 75
+    Height = 25
+    Caption = 'Calculate'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 215
+    Top = 304
+    Width = 75
+    Height = 25
+    Caption = 'Close'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+end
