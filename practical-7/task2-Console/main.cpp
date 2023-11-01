6f52f42be90c8c372330450acbf434cd646b500c
Oleh Donets <qyjodixilu@gmail.com>
2023-11-01 14:30:55 +0200

    practical 7 task 2c comments added

--- practical-7/task2-Console/main.cpp
+++ practical-7/task2-Console/main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <cmath>
+#include <cmath>     // Включення бібліотеки для математичних функцій
 
-using namespace std;
+using namespace std; // Використання простору імен std
 
 int main() {
+    // ініціалізація змінних
     double i = 5;
     double a = 0;
     double b = 1;
 
+    // Розрахунок за формулою суми
     for (int k = 1; k <= i+8; k++) {
-        double ak = cos(pow(k, 3) + 1) - fabs(sin(2 * k)- 5.5);
+        double ak = cos(pow(k, 3) + 1) - fabs(sin(2 * k) - 5.5);
         a += ak;
     }
 
+    // Розрахунок за формулою добутку
     for (int k = 1; k <= i+4; k++) {
         double bk = sin(k) - cos(pow(k, 3)) * sin(pow(k, 2) - 4.24) + 4.24;
-        b *= bk;
+        b *= bk;  // Множення 'bk' на поточне значення 'b'.
     }
 
-    double z = pow(0.1 * b, 2);
+    double z = pow(0.1 * b, 2);  // Обчислення змінної 'z'.
 
-    cout << i << endl;
-    cout << a << endl;
-    cout << b << endl;
-    cout << z << endl;
+    // Виведення результатів на екран.
+    cout << "i = " << i << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "z = " << z << endl;
 
     return 0;
 }
