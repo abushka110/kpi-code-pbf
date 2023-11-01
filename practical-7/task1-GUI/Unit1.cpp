25d689d3b3fe26adf48b3239db64b712aa721286
Oleh Donets Win <qyjodixilu@gmail.com>
2023-11-01 19:24:52 +0200

    practical 7 task 1g

--- practical-7/task1-GUI/Unit1.cpp
+++ practical-7/task1-GUI/Unit1.cpp
@@ -18,18 +18,21 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 
 void __fastcall TForm1::Button1Click(TObject *Sender)
 {
-	Memo1->Clear();
+	Memo1->Clear(); // Очищення вмісту Memo1
 
+	// Отримання початкових значень з полів Edit і перетворення їх в тип double
 	double startValue = StrToFloat(Edit1->Text);
 	double stepSize = StrToFloat(Edit2->Text);
 	int rowCount = StrToFloat(Edit3->Text);
 
+	// Додання заголовка до Memo1
 	Memo1->Lines->Add("Standart                  Roda       Cubic Meter");
 
-	for (int i = 0; i < rowCount; i++) {
+	for (int i = 0; i < rowCount; i++) {  // Цикл для обчислення і виведення значень
 		double standard = startValue + i * stepSize;
-		double roda = standard * 0.165;
-		double cubicMeter = standard * 4.672;
+		double roda = standard * 0.165;  // Обчислення значення "standard"
+		double cubicMeter = standard * 4.672;   // Обчислення значення "cubicMeter"
+		// Додання результатів до Memo1
 		Memo1->Lines->Add(FloatToStr(standard) + "                  " + FloatToStr(roda) + "       " + FloatToStr(cubicMeter));
 	}
 
