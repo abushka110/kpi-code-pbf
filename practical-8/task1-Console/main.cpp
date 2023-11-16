8b3443b62adc529f585f6db2c3e07a08a1a70eb7
Oleh Donets <qyjodixilu@gmail.com>
2023-11-16 22:17:13 +0200

    comments

--- practical-8/task1-Console/main.cpp
+++ practical-8/task1-Console/main.cpp
@@ -4,53 +4,59 @@
 
 using namespace std;
 
-double factorial(int k) {
-    if (k == 0 || k == 1) {
+// Функція для обчислення факторіалу заданого цілого числа
+double factorial(int n) {
+    if (n == 0 || n == 1) {
         return 1;
     } else {
-        return k * factorial(k - 1);
+        return n * factorial(n - 1);
     }
 }
 
+// Функція для обчислення кожного члена ряду
 double calculateTerm(int k, double x) {
+    // Формула для члена ряду
     double f = 4.31 + pow(cos(x), 2) * sin(2 * x - 2);
     double factorialK = factorial(k);
     return pow(-1, k) * (f * (2 * k + 1) * pow(x, k) / factorialK);
 }
 
 int main() {
+    // Файловий потік для запису результатів у файл
     ofstream fout;
-    fout.open("output.txt", ios::app);
+    fout.open("result.txt", ios::app);
 
     double x, precision;
     int terms = 0;
     double sum = 0.0;
 
-    // Get user input for x and precision
-    cout << "Enter a value for x (between 0 and 1): ";
+    // Введення користувачем значення x
+    cout << "Введіть значення для x (від 0 до 1): ";
     cin >> x;
 
-    cout << "Enter the desired precision (e.g., 0.001): ";
+    // Введення користувачем бажаної точності
+    cout << "Введіть бажану точність (наприклад, 0.001): ";
     cin >> precision;
 
-    // Calculate the sum of the series
     double term = 0;
+    // Цикл для обчислення суми ряду до досягнення бажаної точності
     while (fabs(term) >= precision || terms == 0) {
         terms++;
         term = calculateTerm(terms, x);
         sum += term;
     }
 
-    // Output the result
-    cout << "Sum of the series with precision " << precision << ": " << sum << endl;
-    cout << "Number of terms summed: " << terms << endl;
+    // Виведення суми ряду та кількості доданків
+    cout << "Сума ряду з точністю " << precision << ": " << sum << endl;
+    cout << "Кількість доданків: " << terms << endl;
 
-    // Write results to the file
-    fout << "For x = " << x << " and precision = " << precision << endl;
-    fout << "Sum of the series: " << sum << endl;
-    fout << "Number of terms summed: " << terms << endl;
+    // Запис результатів у файл
+    fout << "Для x = " << x << " та точності = " << precision << endl;
+    fout << "Сума ряду: " << sum << endl;
+    fout << "Кількість доданків: " << terms << endl;
     fout << "----------------------------------------\n";
 
+    // Закриття файлового потоку
     fout.close();
 
     return 0;
