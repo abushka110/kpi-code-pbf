922e21b2fd36cad17d3ca035ce6700c3b386513b
abushka110 <qyjodixilu@gmail.com>
2023-12-02 05:50:51 +0200

    comments

--- practical-8/task1-Console/main.cpp
+++ practical-8/task1-Console/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// Функція для обчислення факторіалу числа n
 double factorial(int n) {
     double fact = 1;
     for(int i = 1; i <= n; ++i) {
@@ -13,19 +14,26 @@ double factorial(int n) {
 }
 
 int main() {
+    // Оголошення змінних
     double x, e, a, f, sum = 0;
     int k = 1;
 
+    // Введення значення x
     cout << "Введіть x (0 <= x <= 1): ";
     cin >> x;
+
+    // Введення точності e
     cout << "Введіть точність e: ";
     cin >> e;
 
+    // Відкриття файлу для запису результатів
     ofstream fout;
     fout.open("file.txt", std::ios::app);
 
+    // Обчислення значення f
     f = 4.31 + pow(cos(x), 2) * sin(2*x - 2);
 
+    // Обчислення та сумування доданків у ряду
     a = pow(-1, k) * ((f*(2*k+1)* pow(x, k))/ factorial(k));
     while (fabs(a) >= e) {
         sum += a;
@@ -33,12 +41,15 @@ int main() {
         a = pow(-1, k) * ((f*(2*k+1)* pow(x, k))/ factorial(k));
     }
 
+    // Запис результатів у файл
     fout << "Сума ряду: " << sum << "\n";
     fout << "Кількість доданків, що підсумовуються: " << k << "\n";
 
+    // Виведення результатів на консоль
     cout << "Сума ряду: " << sum << "\n";
     cout << "Кількість доданків, що підсумовуються: " << k << "\n";
 
+    // Закриття файлу
     fout.close();
 
     return 0;
