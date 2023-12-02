6824cba34f2b39b141de3d8052b5f724e926146e
Oleh Donets Win <qyjodixilu@gmail.com>
2023-12-02 05:15:33 +0200

    p8 t1 GUI

---
+++ practical-8/task1-GUI/Unit1.cpp
@@ -0,0 +1,50 @@
+﻿//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#include <cmath>
+#include <fstream>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+double factorial(int n) {
+    double x, e, a, f, sum = 0;
+    int k = 1;
+
+    cout << "Введіть x (0 <= x <= 1): ";
+    cin >> x;
+    cout << "Введіть точність e: ";
+    cin >> e;
+
+    ofstream fout;
+    fout.open("file.txt", std::ios::app);
+
+    f = 4.31 + pow(cos(x), 2) * sin(2*x - 2);
+
+    a = pow(-1, k) * ((f*(2*k+1)* pow(x, k))/ factorial(k));
+    while (fabs(a) >= e) {
+        sum += a;
+        k++;
+        a = pow(-1, k) * ((f*(2*k+1)* pow(x, k))/ factorial(k));
+    }
+
+    fout << "Сума ряду: " << sum << "\n";
+    fout << "Кількість доданків, що підсумовуються: " << k << "\n";
+
+    cout << "Сума ряду: " << sum << "\n";
+    cout << "Кількість доданків, що підсумовуються: " << k << "\n";
+
+    fout.close();
+
+    return 0;
+}
+//---------------------------------------------------------------------------
---
+++ practical-8/task1-GUI/Unit1.dfm
@@ -0,0 +1,57 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 212
+  ClientWidth = 238
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 32
+    Top = 16
+    Width = 115
+    Height = 15
+    Caption = 'Enter x (0 <= x <= 1): '
+  end
+  object Label2: TLabel
+    Left = 33
+    Top = 78
+    Width = 93
+    Height = 15
+    Caption = 'Enter precision e: '
+  end
+  object Button1: TButton
+    Left = 32
+    Top = 137
+    Width = 161
+    Height = 25
+    Caption = 'Calculate'
+    TabOrder = 0
+  end
+  object Edit1: TEdit
+    Left = 33
+    Top = 37
+    Width = 160
+    Height = 23
+    TabOrder = 1
+  end
+  object Edit2: TEdit
+    Left = 33
+    Top = 99
+    Width = 160
+    Height = 23
+    TabOrder = 2
+  end
+  object Panel1: TPanel
+    Left = 32
+    Top = 168
+    Width = 161
+    Height = 41
+    TabOrder = 3
+  end
+end
