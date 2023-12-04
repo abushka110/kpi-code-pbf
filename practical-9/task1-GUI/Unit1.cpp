b030084033f1bd8846161029c26428eaff327934
Oleh Donets Win <qyjodixilu@gmail.com>
2023-12-04 19:54:45 +0200

    minor changes

--- practical-9/task1-GUI/Unit1.cpp
+++ practical-9/task1-GUI/Unit1.cpp
@@ -22,43 +22,34 @@ void __fastcall TForm1::CalculateBtnClick(TObject *Sender)
     const int startYear = 2011;
     const int endYear = 2022;
     const int numYears = endYear - startYear + 1;
-
     // Масив для зберігання виробництва протягом кожного року
     double production[numYears];
-
     // Обчислення виробництва для кожного року за заданою формулою
     for (int k = 0; k < numYears; ++k) {
         double year = startYear + k;
         production[k] = 100 * (2 * sin(year) * sin(4 * year - 1.4) * cos(2 * year + 1.5) - 7.1);
     }
-
 	// Виведення заголовка таблиці
-	Memo1->Lines->Add("|   Year    |    Number of products (thousands)   |");
-
+	Memo1->Lines->Add("|     Year      |    Number of products (thousands)   |");
     // Ініціалізація змінних для підрахунку статистики
     int profitableYearCount = 0;
     int maxProductionYear = 0;
     double maxProduction = 0;
 	double lossProductionSum = 0;
 	int lossProductionSumInt = 0;
-
     // Виведення даних про виробництво для кожного року
     for (int k = 0; k < numYears; ++k) {
         int year = startYear + k;
 		double currentProduction = production[k];
-
 		int currentProductionInt = (int)currentProduction;
-
-        // Виведення даних у вигляді таблиці
-		Memo1->Lines->Add("|     " + FloatToStr(year) + "     |            " + IntToStr(currentProductionInt) + "             |");
-
+		// Виведення даних у вигляді таблиці
+		Memo1->Lines->Add("|     " + FloatToStr(year) + "     |                            " + IntToStr(currentProductionInt) + "                             |");
 		// Підрахунок кількості років з прибутком та суми збиткових років
         if (currentProduction > 0) {
             profitableYearCount++;
         } else {
 			lossProductionSum += currentProduction;
-        }
-
+		}
         // Знаходження року з найбільшим виробництвом
         if (std::abs(currentProduction) > std::abs(maxProduction)) {
             maxProduction = currentProduction;
@@ -66,9 +57,7 @@ void __fastcall TForm1::CalculateBtnClick(TObject *Sender)
 		}
         lossProductionSumInt = (int)lossProductionSum;
     }
-
 	Memo1->Lines->Add("----------------------------------------------");
-
     // Виведення статистики
 	Memo1->Lines->Add("Total number of years with profit: " + FloatToStr(profitableYearCount));
 	Memo1->Lines->Add("The sum of the number of devices for unprofitable years: " + IntToStr(lossProductionSumInt));
