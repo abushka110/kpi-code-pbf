24c03cc7be3fba6e9e8361557894bbfc517918ca
abushka110 <qyjodixilu@gmail.com>
2023-12-06 14:48:45 +0200

    minor changes

--- practical-10/task1-Console/main.cpp
+++ practical-10/task1-Console/main.cpp
@@ -20,37 +20,6 @@ struct Employee {
     double tax; // Податок
 };
 
-// Функція для генерації відпрацьованих днів та запису їх у файл
-void generateData(Employee arr[], int size, ofstream& file) {
-    // Створення об'єкта для генерації випадкових чисел
-    random_device rd;
-    // Ініціалізація генератора випадкових чисел Mersenne Twister з випадковим початковим значенням
-    mt19937 gen(rd());
-    // Створення рівномірного розподілу випадкових чисел від 0 до 31
-    uniform_int_distribution<> dis(0, 31);
-
-    int totalDays = 0; // Загальна кількість відпрацьованих днів
-
-    // Заповнення масиву даними про працівників
-    for (int i = 0; i < size; i++) {
-        // Генерація випадкового числа — кількість відпрацьованих днів
-        arr[i].days = dis(gen);
-
-        // Підрахунок загальної кількості відпрацьованих днів для всіх працівників
-        totalDays += arr[i].days;
-
-        // Розрахунок зарплати та податку для кожного працівника
-        arr[i].salary = arr[i].days * salaryPerDay;
-        arr[i].tax = arr[i].salary * taxRate;
-
-        // Запис інформації про працівника у файл
-        file << "Робітник: " << i+1 << ", Днів: " << arr[i].days << ", Зарплата: " << arr[i].salary << ", Податок: " << arr[i].tax << endl;
-    }
-
-    // Запис загальної кількості відпрацьованих днів у файл
-    file << "Всього відпрацьовано днів: " << totalDays << endl << endl;
-}
-
 int main() {
     // Створення масивів для працівників у підрозділах A та B
     Employee employeesA[A], employeesB[B];
@@ -58,16 +27,48 @@ int main() {
     // Відкриття файлу для запису
     ofstream file("data.txt");
 
+    // Створення об'єкта для генерації випадкових чисел
+    random_device rd;
+    // Ініціалізація генератора випадкових чисел Mersenne Twister з випадковим початковим значенням
+    mt19937 gen(rd());
+    // Створення рівномірного розподілу випадкових чисел від 0 до 31
+    uniform_int_distribution<> dis(0, 31);
+
     // Запис інформації про підрозділ A у файл
     file << "Підрозділ A:\n";
-    generateData(employeesA, A, file);
+    int totalDaysA = 0; // Загальна кількість відпрацьованих днів
+    for (int i = 0; i < A; i++) {
+        // Генерація випадкового числа — кількість відпрацьованих днів
+        employeesA[i].days = dis(gen);
+        // Підрахунок загальної кількості відпрацьованих днів для всіх працівників
+        totalDaysA += employeesA[i].days;
+        // Розрахунок зарплати та податку для кожного працівника
+        employeesA[i].salary = employeesA[i].days * salaryPerDay;
+        employeesA[i].tax = employeesA[i].salary * taxRate;
+        // Запис інформації про працівника у файл
+        file << "Робітник: " << i+1 << ", Днів: " << employeesA[i].days << ", Зарплата: " << employeesA[i].salary << ", Податок: " << employeesA[i].tax << endl;
+    }
+    // Запис загальної кількості відпрацьованих днів у файл
+    file << "Всього відпрацьовано днів: " << totalDaysA << endl << endl;
 
     // Запис інформації про підрозділ B у файл
     file << "Підрозділ B:\n";
-    generateData(employeesB, B, file);
+    int totalDaysB = 0; // Загальна кількість відпрацьованих днів
+    for (int i = 0; i < B; i++) {
+        // Генерація випадкового числа — кількість відпрацьованих днів
+        employeesB[i].days = dis(gen);
+        // Підрахунок загальної кількості відпрацьованих днів для всіх працівників
+        totalDaysB += employeesB[i].days;
+        // Розрахунок зарплати та податку для кожного працівника
+        employeesB[i].salary = employeesB[i].days * salaryPerDay;
+        employeesB[i].tax = employeesB[i].salary * taxRate;
+        // Запис інформації про працівника у файл
+        file << "Робітник: " << i+1 << ", Днів: " << employeesB[i].days << ", Зарплата: " << employeesB[i].salary << ", Податок: " << employeesB[i].tax << endl;
+    }
+    // Запис загальної кількості відпрацьованих днів у файл
+    file << "Всього відпрацьовано днів: " << totalDaysB << endl << endl;
 
     // Закриття файлу
     file.close();
     return 0;
 }
-
