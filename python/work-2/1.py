a6041fe43d732afbae9348dc44d029239701052f
abushka110 <qyjodixilu@gmail.com>
2024-03-11 11:29:07 +0200

    minor changes

--- python/work-2/1.py
+++ python/work-2/1.py
@@ -1,8 +1,8 @@
 x = int(input("Введіть значення x: "))
 
-if x >= -4:
+if x >= -4 and x != -1:
     print((x**2 - 1) / (pow(x, 3) + 1))
-elif x < -8:
+elif x < -8 and x != -1:
     print((x + 3) * (x - 2))
 else:
     print(f"Значення x({x}) не входить в область допустимих значень")
