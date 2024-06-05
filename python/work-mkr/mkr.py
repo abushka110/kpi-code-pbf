a5c1f896cafd988927127ef53994832f92621803
abushka110 <qyjodixilu@gmail.com>
2024-06-05 14:06:53 +0200

    Frames for buttons

--- python/work-mkr/mkr.py
+++ python/work-mkr/mkr.py
@@ -20,23 +20,32 @@ def clear_plot():
 root = tk.Tk()
 
 fig = Figure(figsize=(5, 4), dpi=100)  # фігура для графіку
-ax = fig.add_subplot(111)  # осі до фігури
+# осі до фігури, перша цифра - кількість рядків, друга - кількість стовпців, третя - індекс поточного subplot в сітці
+ax = fig.add_subplot(111)  
 canvas = FigureCanvasTkAgg(fig, master=root)  # полотно для відображення фігури
 canvas.draw()
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 color_var = tk.StringVar(value='blue')  # змінна для зберігання вибраного кольору
 
+# Frame для radio buttons
+radio_frame = tk.Frame(root)
+radio_frame.pack(side = tk.LEFT)
+
 # radio buttons для вибору кольору
 colors = ['yellow', 'green', 'blue', 'red', 'brown', 'black']
 for color in colors:
-    color_rb = tk.Radiobutton(root, text=color, variable=color_var, value=color)
+    color_rb = tk.Radiobutton(radio_frame, text=color, variable=color_var, value=color)
     color_rb.pack()
 
-plot_button = tk.Button(root, text="Побудувати графік", command=plot_sine)  # кнопка для побудови графіку
-plot_button.pack() # додає кнопку до головного екрану
+# Frame для кнопок графіку
+buttons_frame = tk.Frame(root)
+buttons_frame.pack(side = tk.RIGHT)
 
-clear_button = tk.Button(root, text="Очистити графік", command=clear_plot)  # кнопка для очищення графіку
+plot_button = tk.Button(buttons_frame, text="Побудувати графік", command=plot_sine)  # кнопка для побудови графіку
+plot_button.pack() # додає кнопку до Frame
+
+clear_button = tk.Button(buttons_frame, text="Очистити графік", command=clear_plot)  # кнопка для очищення графіку
 clear_button.pack()
 
 tk.mainloop()  # запуск основного циклу обробки подій tkinter
